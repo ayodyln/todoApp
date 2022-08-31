29a9d11f3a00981a0a89c9577cf521b3bf0b2fbe
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-08-31 13:13:01 -0600

    update to functions

--- app.js
+++ app.js
@@ -19,14 +19,41 @@ const todoModel = {
 //TODO:
 
 //? Function Calls
-await createTodo(myTodos, todoModel, { title: "Hello World" });
-await createNewCategory(myTodos[2]);
-await editTodo(myTodos[2], {
-  // titleInput: "New Title",
-  statusInput: true,
-  categoryInput: ["general", "school"],
-  // dateInput: 50000,
-});
+// await createTodo(myTodos, todoModel, { title: "Hello World" });
+// await createNewCategory(myTodos[2]);
+// await editTodo(myTodos[2], {
+//   // titleInput: "New Title",
+//   statusInput: true,
+//   categoryInput: ["general", "school"],
+//   // dateInput: 50000,
+// });
 
 //! Logging
 console.log(myTodos);
+
+//? Section
+
+//? Button Binding
+const createTodoButton = document.querySelector("#createTodoBtn");
+const createCategoryButton = document.querySelector("#createNewCategory");
+const editTodoButton = document.querySelector("#editTodo");
+
+createTodoButton.addEventListener("click", async () => {
+  await createTodo(myTodos, todoModel, {
+    title: "Hello JS World",
+  });
+  console.log(myTodos);
+});
+
+createCategoryButton.addEventListener("click", async () => {
+  await createNewCategory(myTodos[1], ["Test", "Oof"]);
+  console.log(myTodos);
+});
+
+editTodoButton.addEventListener("click", async () => {
+  await editTodo(myTodos[1], {
+    titleInput: "foo",
+    statusInput: true,
+  });
+  console.log(myTodos);
+});
