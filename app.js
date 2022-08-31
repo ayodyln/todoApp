0a5a052f66d018f40c72fccb1cbd192ad0760a77
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-08-30 21:42:29 -0600

    update to assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+import myTodos from "./data/todos.js";
+import {
+  createNewCategory,
+  createTodo,
+  editTodo,
+} from "./lib/TodoFunctions.js";
+
+//! Data Modal -- DONT EDIT
+const todoModel = {
+  id: NaN,
+  title: "",
+  status: false || true,
+  category: [],
+  startDate: new Date(),
+  dueDate: new Date(),
+};
+
+//? Function Calls
+await createTodo(myTodos, todoModel, { title: "Hello World" });
+await createNewCategory(myTodos[2]);
+await editTodo(myTodos[2], {
+  titleInput: "New Title",
+  statusInput: true,
+  categoryInput: ["general", "school"],
+});
+
+//! Logging
+console.log(myTodos);
---
+++ data/todos.js
@@ -0,0 +1,21 @@
+//? Data Array
+const myTodos = [
+  {
+    id: 0,
+    title: "First Todo",
+    status: false,
+    category: ["Example"],
+    startDate: new Date(),
+    dueDate: new Date(),
+  },
+  {
+    id: 1,
+    title: "Second Todo",
+    status: false,
+    category: ["Example"],
+    startDate: new Date(),
+    dueDate: new Date(),
+  },
+];
+
+export default myTodos;
